8b995d4be96e7953c325efeefb8bcc1a3ced1d53
Blaise Ritchie <blaise@jizzyjugs.com>
2013-02-07 17:16:40 -0800

    fixed advanced search options

--- static/js/script.js
+++ static/js/script.js
@@ -2,7 +2,7 @@ google.load("jquery", "1.6.4");
 google.setOnLoadCallback(function() {
 
 	$("#searchb").click(function () {
-		window.location.href = "/" + $("#filter").val() + "/" + $("#category") + "/search/" + $("#search").val();
+		window.location.href = "/" + $("#filter").val() + "/" + $("#category").val() + "/search/" + $("#search").val();
 	});
 
 });

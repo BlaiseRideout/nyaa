6bad6b616c09c39c6d38b48c450778ad990aa666
Blaise Ritchie <blaise@jizzyjugs.com>
2013-02-07 18:21:29 -0800

    enabled enter for searching and fixed page width

--- static/js/script.js
+++ static/js/script.js
@@ -1,8 +1,10 @@
 google.load("jquery", "1.6.4");
 google.setOnLoadCallback(function() {
+	$("#searchf").submit(function (event) {
+		event.preventDefault();
 
-	$("#searchb").click(function () {
 		window.location.href = "/" + $("#filter").val() + "/" + $("#category").val() + "/search/" + $("#search").val();
-	});
 
+		return false;
+	});
 });
